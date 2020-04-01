cdb0ece39e1043ef1d9c25fac592a69e247e1e61
Regner Aguiar (Valtech) <regner.aguiar@valtech.com>
2020-04-01 11:34:02 -0300

    Code Refactory

--- tests/TDD.Original.Tests/CadastroCliente/CadastroCliente.cs
+++ tests/TDD.Original.Tests/CadastroCliente/CadastroCliente.cs
@@ -1,9 +1,9 @@
 ﻿using FluentAssertions;
 using Moq;
-using Moq.AutoMock;
 using System;
 using System.Linq;
 using TDD.Original.Tests.CadastroCliente.Fixtures;
+using TDD.Original.Web.Models;
 using TDD.Original.Web.Repository;
 using TDD.Original.Web.Service;
 using Xunit;
@@ -13,73 +13,57 @@ namespace TDD.Original.Tests.CadastroCliente
     [Collection(nameof(ClienteCollection))]
     public class CadastroCliente : IDisposable
     {
-        private readonly ClienteTestFixture _clienteTestFixture;    
+        private readonly CadastroClienteFixture _cadastroClienteFixture;
+        private readonly ClienteService _clienteService;
+        private readonly Mock<IClienteRepository> _clienteRepository;
+        private readonly Cliente _clienteValido;
+        private readonly Cliente _clienteInvalido;
 
-        public CadastroCliente(ClienteTestFixture clienteTestFixture)
+        public CadastroCliente(CadastroClienteFixture clienteTestFixture)
         {
-            _clienteTestFixture = clienteTestFixture;
+            _cadastroClienteFixture = clienteTestFixture;
+            _clienteValido = _cadastroClienteFixture.GerarClienteValido();
+            _clienteInvalido = _cadastroClienteFixture.GerarClienteInvalido();
+            _clienteService = _cadastroClienteFixture.ObterClienteService();
+            _clienteRepository = _cadastroClienteFixture.Mocker.GetMock<IClienteRepository>();
         }
 
         [Fact(DisplayName = "Cliente válido")]
         [Trait("Categoria", "Cadastro de Clientes")]
         public void CadastroCliente_AdicionarCliente_EhValida()
-        {            
-            // Act
-            var ehValida = _clienteTestFixture.GerarClienteValido().EhValida();
-
-            // Assert
-            Assert.True(ehValida);
+        {
+            // Act && Assert
+            Assert.True(_clienteValido.EhValida());
         }
 
         [Fact(DisplayName = "Cliente não é válido")]
         [Trait("Categoria", "Cadastro de Clientes")]
         public void CadastroCliente_AdicionarCliente_NaoValida()
         {
-            // Arrange
-            var clienteInvalido = _clienteTestFixture.GerarClienteInvalido();
-            
-                // Act
-            var ehValida = clienteInvalido.EhValida();
-
-            // Assert
-            Assert.False(ehValida);
-            Assert.True(clienteInvalido.ValidationResult.Errors.Any());
+            // Act && Assert
+            Assert.False(_clienteInvalido.EhValida());
+            Assert.True(_clienteInvalido.ValidationResult.Errors.Any());
         }
 
         [Fact(DisplayName = "Cadastrado com sucesso")]
-        [Trait("Categoria", "Cadastro de Clientes")]        
+        [Trait("Categoria", "Cadastro de Clientes")]
         public void CadastroCliente_AdicionarCliente_ComSucesso()
         {
             // Arrange
-            var pessoa = _clienteTestFixture.GerarClienteValido();
-            var mocker = new AutoMocker();
-            var _clienteService = mocker.CreateInstance<ClienteService>();
-            var _clienteRepository = mocker.GetMock<IClienteRepository>();
-            _clienteRepository.Setup(x => x.Adicionar(pessoa)).Returns(true);
-
-            // Act
-            var persistido = _clienteService.Adicionar(pessoa);
-
-            // Assert
-            Assert.True(persistido);
-            persistido.Should().BeTrue();
-            _clienteRepository.Verify(x => x.Adicionar(pessoa), Times.Once);
+            _clienteRepository.Setup(x => x.Adicionar(_clienteValido)).Returns(true);
+            
+            // Act && Assert
+            _clienteService.Adicionar(_clienteValido).Should().BeTrue();
+            _clienteRepository.Verify(x => x.Adicionar(_clienteValido), Times.Once);
         }
 
         [Fact(DisplayName = "Cadastrado sem sucesso")]
         [Trait("Categoria", "Cadastro de Clientes")]
         public void CadastroCliente_AdicionarCliente_SemSucesso()
         {
-            // Arrange
-            var pessoa = _clienteTestFixture.GerarClienteInvalido();
-            var _clienteService = new Mock<IClienteService>();
-
-            // Act
-            var persistido = _clienteService.Object.Adicionar(pessoa);
-
-            // Assert
-            Assert.False(persistido);
-            _clienteService.Verify(x => x.Adicionar(pessoa), Times.Once);
+            // Act && Assert
+            Assert.False(_clienteService.Adicionar(_clienteInvalido));
+            _clienteRepository.Verify(x => x.Adicionar(_clienteInvalido), Times.Never);
         }
 
         public void Dispose()
--- tests/TDD.Original.Tests/CadastroCliente/Fixtures/CadastroClienteFixture.cs
+++ tests/TDD.Original.Tests/CadastroCliente/Fixtures/CadastroClienteFixture.cs
@@ -1,18 +1,23 @@
 ﻿using Bogus;
 using Bogus.Extensions.Brazil;
+using Moq.AutoMock;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using TDD.Original.Web.Models;
+using TDD.Original.Web.Service;
 using Xunit;
 
 namespace TDD.Original.Tests.CadastroCliente.Fixtures
 {
     [CollectionDefinition(nameof(ClienteCollection))]
-    public class ClienteCollection : ICollectionFixture<ClienteTestFixture>
+    public class ClienteCollection : ICollectionFixture<CadastroClienteFixture>
     { }
-    public class ClienteTestFixture
+    public class CadastroClienteFixture
     {
         public List<Cliente> Clientes { get; set; }
+        public ClienteService ClienteService;
+        public AutoMocker Mocker;
 
         public Cliente GerarClienteInvalido()
         {
@@ -20,13 +25,28 @@ namespace TDD.Original.Tests.CadastroCliente.Fixtures
         }
         public Cliente GerarClienteValido()
         {
-            return new Faker<Cliente>("pt_BR")
+            return GerarClientes(1).FirstOrDefault();
+        }
+
+        public List<Cliente> GerarClientes(int quantidade)
+        {
+            var clienteFaker = new Faker<Cliente>("pt_BR")
                 .CustomInstantiator(x => new Cliente(
                     x.Person.FullName,
                     x.Person.Cpf(),
                     x.Date.Past(100, DateTime.Now.AddYears(-18)),
                     x.Address.FullAddress()
                     ));
+
+            return clienteFaker.Generate(quantidade);
+        }
+        
+        public ClienteService ObterClienteService()
+        {
+            Mocker = new AutoMocker();
+            ClienteService = Mocker.CreateInstance<ClienteService>();
+
+            return ClienteService;
         }
     }
 }
